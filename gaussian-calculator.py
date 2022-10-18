d55f31101e734def9b1bbd8bf8f4232fa33c99dd
Sonsawan <sonsawan.n1994@gmail.com>
2022-10-18 19:39:05 +0700

    "V1.0"

---
+++ gaussian-calculator.py
@@ -0,0 +1,34 @@
+import math
+import matplotlib.pyplot as plt
+
+class Gaussian():
+    """ Gaussian distribution class for calculating and 
+    visualizing a Gaussian distribution.
+    
+    Attributes:
+        mean (float) representing the mean value of the distribution
+        stdev (float) representing the standard deviation of the distribution
+        data_list (list of floats) a list of floats extracted from the data file
+            
+    """
+    def __init__(self, mu = 0, sigma =1):
+        
+        self.mean = mu
+        self.stdev = sigma
+        self.data = []
+        
+    
+    def calculate_mean(self):
+    """Method to calculate the mean of the data set.
+        
+        Args: 
+            None
+        
+        Returns: 
+            float: mean of the data set
+    
+    """
+        arr = self.data
+        
+
+            
